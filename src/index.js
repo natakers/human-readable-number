77934979b32bc9043ff8a6779b0a7642590c6419
natakers <n.kersnovskaya@gmail.com>
2021-08-03 21:24:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ module.exports = function toReadable (number) {
 	const arr = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve', 'thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
 	const arrDozens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
 	const arrHundred = ['', 'one hundred', 'two hundred','three hundred','four hundred','five hundred', 'six hundred','seven hundred','eight hundred','nine hundred'];
+	if (number == 0) {
+		// n=n/100;
+		str = 'zero';
+	}
 	if (number<20) {
 		// n=n/100;
 		str = str + arr[number];
 	}
-	if (number<=100 && number>=20) {
+	if (number<100 && number>=20) {
 		// n=n/100;
 		str = str + arrDozens[Math.floor(number/10)]; 
 		if (number%10 != 0) {
 			str = str + " " + arr[number%10];
 		} 
 	}
-	if (number<=999 && number>100) {
+	if (number<=999 && number>=100) {
 		// n=n/100;
 		str = str + arrHundred[Math.floor(number/100)];
 		if (Math.floor(number%100) < 20 && Math.floor(number%100)!= 0) {
