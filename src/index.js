3b5c86aecea572b964918b397cfff82ead2335c0
natakers <n.kersnovskaya@gmail.com>
2021-08-03 21:23:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ module.exports = function toReadable (number) {
 			str = str + " " + arr[number%10];
 		} 
 	}
-	if (number<999 && number>100) {
+	if (number<=999 && number>100) {
 		// n=n/100;
 		str = str + arrHundred[Math.floor(number/100)];
-		if (Math.floor(number%100) < 20) {
-			str = str + " " + arr[number];
+		if (Math.floor(number%100) < 20 && Math.floor(number%100)!= 0) {
+			str = str + " " + arr[number%100];
 		}
 		if (Math.floor(number%100) >= 20){
 			str = str + " " + arrDozens[Math.floor(number%100/10)];
